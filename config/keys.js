80c88ea4ae72b49f007aaea00ffe83705bbe0d8a
Prakash Mishra <prakashmishra1108@gmail.com>
2018-09-10 01:00:27 +0530

    updated

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 //add this file to git ignore
 module.exports = {
   google:{
-    clientID:'1060746957892-kjr1bp7q5orsvd14pjhk3niljaqj7r4h.apps.googleusercontent.com',
-    clientSecret:'sNjf86bWDnzFBfPvuC5sBfAG'
+    clientID:'291045893578-j1ihfnm2i8bsg4cg0n3f7m5f7thovob7.apps.googleusercontent.com',
+    clientSecret:'haMnEU4E_uLgRYwSQTcS2VlJ'
   },
   mongodb:{
     dbURI:'mongodb://prakash:prakash1108@ds151012.mlab.com:51012/slp'
