3da3cca17d6ffb0744cce99199f513b43f385739
Mohammad Ahmad <ahmadofficial242@gmail.com>
2018-10-05 00:18:48 +0530

    source code option is added and sidebar is added in contact.ejs

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const authRoutes = require('./routes/auth-routes');
-const profileRoutes = require('./routes/profile-routes');
-const passportSetup = require('./config/passport-setup');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const Contact = require('./models/contact-model');
-const Notice = require('./models/notice-model');
-var bodyParser = require('body-parser');
-
-const app = express();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const express = require('express'),
+  authRoutes = require('./routes/auth-routes'),
+  profileRoutes = require('./routes/profile-routes'),
+  passportSetup = require('./config/passport-setup'),
+  mongoose = require('mongoose'),
+  keys = require('./config/keys'),
+  cookieSession = require('cookie-session'),
+  passport = require('passport'),
+  Contact = require('./models/contact-model'),
+  Notice = require('./models/notice-model'),
+  bodyParser = require('body-parser'),
+  app = express()
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
 
 //set up view engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //static files
 app.use(express.static('./public'));
 
 app.use(cookieSession({
-  maxAge:24*60*60*1000,
-  keys:[keys.session.cookieKey]
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
 }));
 
 //initialise passport
@@ -29,7 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //connect to mongodb
-mongoose.connect(keys.mongodb.dbURI, function(){
+mongoose.connect(keys.mongodb.dbURI, function () {
   console.log('connected to mongodb');
 });
 
@@ -39,49 +40,58 @@ app.use('/profile', profileRoutes);
 
 //=====================================================================
 //create home route
-app.get('/', function(req, res){
-    //get data from mongodb and pass it to view
-    Notice.find({}, function (err, data) {
-      if(err) throw err;
-      res.render('home', {user: req.user ,notices: data});
-    })
-  });
-
-  app.post('/', urlencodedParser, function(req, res){
-    //get data from the view and it mongodb
-    var newNotice = Notice(req.body).save(function (err, data) {
-      if (err) throw err;
-      res.json(data);
+app.get('/', function (req, res) {
+  //get data from mongodb and pass it to view
+  Notice.find({}, function (err, data) {
+    if (err) throw err;
+    res.render('home', {
+      user: req.user,
+      notices: data
     });
-  });
+  })
+});
 
-  app.delete('/:item', function(req, res){
-    //delete the requested item from mongodb
-    Notice.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err,data) {
-      if(err) throw err;
-      res.json(data);
-    });
+app.post('/', urlencodedParser, function (req, res) {
+  //get data from the view and it mongodb
+  var newNotice = Notice(req.body).save(function (err, data) {
+    if (err) throw err;
+    res.json(data);
   });
+});
+
+app.delete('/:item', function (req, res) {
+  //delete the requested item from mongodb
+  Notice.find({
+    item: req.params.item.replace(/\-/g, " ")
+  }).remove(function (err, data) {
+    if (err) throw err;
+    res.json(data);
+  });
+});
 //======================================================================
 
-app.get('/contact',function (req, res) {
-  res.render('contact');
+app.get('/contact', function (req, res) {
+  res.render('contact', {
+    user: req.user
+  });
 });
 
 app.post('/contact', urlencodedParser, function (req, res) {
   console.log(req.body);
   new Contact({
-      email:req.body.email,
-      message:req.body.message,
-      branch:req.body.branch,
-      year:req.body.year
+    email: req.body.email,
+    message: req.body.message,
+    branch: req.body.branch,
+    year: req.body.year
   }).save();
-  res.render('contact-success', {data: req.body});
+  res.render('contact-success', {
+    data: req.body
+  });
 });
 
 
 
 
-app.listen(process.env.PORT || 3000,function(){
-  console.log("listenning for the request");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("listenning for the request on 3000");
 });
